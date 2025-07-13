2811a3a5ce16d0e9719544e4e0d4dcb964a78a75
Oliver A. Kelton <oakelton@gmail.com>
2025-07-13 16:10:46 -0700

    More robust example that releases lock approximately three seconds after last input from client

--- WebSocketServer/src/main.rs
+++ WebSocketServer/src/main.rs
@@ -27,40 +27,61 @@ enum ClientMessage {
     ReleaseLock,
 }
 
+#[derive(Copy,Clone)]
+struct SharedTextLockData {
+    owner_id: u64,
+    duration_secs: u32
+}
+
 type SharedText = Arc<Mutex<String>>;
 type SharedClientId = Arc<Mutex<u64>>;
-type LockOwnerId = Arc<Mutex<Option<u64>>>;
+type SharedTextLock = Arc<Mutex<Option<SharedTextLockData>>>;
 
 #[tokio::main]
 async fn main() {
-    let lock_owner_id = Arc::new(Mutex::new(Option::<u64>::None));
+    let shared_text_lock = Arc::new(Mutex::new(Option::<SharedTextLockData>::None));
     let client_id = Arc::new(Mutex::new(0u64));
     let text = Arc::new(Mutex::new(String::from("Hello, World!")));
     let (tx, _rx) = broadcast::channel::<ClientMessage>(100);
 
-    // test thread
+    // thread to release lock after idle for five seconds
     {
-        let lock_owner_id_clone = Arc::clone(&lock_owner_id);
+        let shared_text_lock_clone = Arc::clone(&shared_text_lock);
         let tx_clone = tx.clone();
 
         let reset_lock = async move || {
            loop {
-                eprintln!("Resetting lock ...");
                 {
-                    let mut lock_owner_id = lock_owner_id_clone.lock().await;
-                    let _ = tx_clone.send(ClientMessage::ReleaseLock);
-                    *lock_owner_id = None;
+                    let mut shared_text_lock = shared_text_lock_clone.lock().await;
+                    let mut to_reset = false;
+
+                    match *shared_text_lock {
+                        None => {},
+                        Some(ref mut info) => {
+                            if info.duration_secs < 2 {
+                                to_reset = true;
+                            } else {
+                                info.duration_secs -= 1;
+                            }
+                        }
+                    };// end match shared_text_lock
+
+                    if to_reset {
+                        eprintln!("Resetting lock ...");
+                        let _ = tx_clone.send(ClientMessage::ReleaseLock);
+                        *shared_text_lock = None;
+                    }
                 }
-                thread::sleep(Duration::from_millis(5000));
+                thread::sleep(Duration::from_millis(1000));
             };
         };
 
         thread::spawn(move || block_on(reset_lock()));
     }
 
-    let lock_owner_id_filter = warp::any().map({
-        let lock_owner_id = Arc::clone(&lock_owner_id);
-        move || Arc::clone(&lock_owner_id)
+    let shared_text_lock_filter = warp::any().map({
+        let shared_text_lock = Arc::clone(&shared_text_lock);
+        move || Arc::clone(&shared_text_lock)
     });
 
     let client_id_filter = warp::any().map({
@@ -80,12 +101,12 @@ async fn main() {
 
     let ws_route = warp::path("ws")
         .and(warp::ws())
-        .and(lock_owner_id_filter)
+        .and(shared_text_lock_filter)
         .and(client_id_filter)
         .and(text_filter)
         .and(tx_filter)
-        .map(|ws: warp::ws::Ws, lock_owner_id, client_id, text, tx| {
-            ws.on_upgrade(move |socket| handle_connection(socket, lock_owner_id, client_id, text, tx))
+        .map(|ws: warp::ws::Ws, shared_text_lock, client_id, text, tx| {
+            ws.on_upgrade(move |socket| handle_connection(socket, shared_text_lock, client_id, text, tx))
         });
 
     let addr: SocketAddr = ([0, 0, 0, 0], 8080).into();
@@ -93,21 +114,25 @@ async fn main() {
     warp::serve(ws_route).run(addr).await;
 }
 
-async fn handle_connection(ws: WebSocket, lock_owner_id: LockOwnerId, client_id: SharedClientId, text: SharedText, tx: broadcast::Sender<ClientMessage>) {
+async fn handle_connection(ws: WebSocket, shared_text_lock: SharedTextLock, client_id: SharedClientId, text: SharedText, tx: broadcast::Sender<ClientMessage>) {
     let (mut user_ws_tx, mut user_ws_rx) = ws.split();
     let mut rx = tx.subscribe();
     let mut current_client_id = u64::MAX;
 
     // Send initial text to the user
     {
-        let lock_owner_id = lock_owner_id.lock().await;
+        let shared_text_lock = shared_text_lock.lock().await;
+        let lock_owner_id : Option<u64> = match *shared_text_lock {
+            None => None,
+            Some(info) => Some(info.owner_id)
+        };
         let mut client_id = client_id.lock().await;
 
         current_client_id = *client_id;
 
         let current_text = text.lock().await.clone();
         let init_msg = ClientMessage::Init{
-            client_id: current_client_id, lock_owner_id: *lock_owner_id, text: current_text
+            client_id: current_client_id, lock_owner_id: lock_owner_id, text: current_text
         };
         let json = serde_json::to_string(&init_msg).unwrap();
         let _ = user_ws_tx.send(Message::text(json)).await;
@@ -130,19 +155,24 @@ async fn handle_connection(ws: WebSocket, lock_owner_id: LockOwnerId, client_id:
         while let Some(Ok(msg)) = user_ws_rx.next().await {
             if let Ok(text_str) = msg.to_str() {
                 if let Ok(mut client_msg) = serde_json::from_str::<ClientMessage>(text_str) {
-                    let mut lock_owner_id = lock_owner_id.lock().await;
+                    let mut shared_text_lock = shared_text_lock.lock().await;
 
-                    match *lock_owner_id {
+                    match *shared_text_lock {
                         None => {
                             // broadcast that a user has acquired the lock
                             let lock_msg = ClientMessage::AcquireLock{ client_id: current_client_id };
                             let _ = tx_clone.send(lock_msg);
 
-                            *lock_owner_id = Some(current_client_id);
+                            *shared_text_lock = Some(SharedTextLockData{
+                                owner_id: current_client_id, duration_secs: 3
+                            });
                             apply_text_change(&client_msg, &text_clone).await;
                             let _ = tx_clone.send(client_msg);
                         },
-                        Some(id) => if current_client_id == id {
+                        Some(ref mut info) => if current_client_id == info.owner_id {
+                            // reset duration
+                            info.duration_secs = 3;
+
                             apply_client_id(&mut client_msg, current_client_id);
                             apply_text_change(&client_msg, &text_clone).await;
                             let _ = tx_clone.send(client_msg);
@@ -154,13 +184,13 @@ async fn handle_connection(ws: WebSocket, lock_owner_id: LockOwnerId, client_id:
 
         // Release lock on client disconnect
         {
-            let mut lock_owner_id = lock_owner_id.lock().await;
+            let mut shared_text_lock = shared_text_lock.lock().await;
 
-            match *lock_owner_id {
+            match *shared_text_lock {
                 None => {},
-                Some(id) => if id == current_client_id {
+                Some(info) => if info.owner_id == current_client_id {
                     let _ = tx_clone.send(ClientMessage::ReleaseLock);
-                    *lock_owner_id = None;
+                    *shared_text_lock = None;
                 }
             };
         }
