e11b29a4669f2ca2d208e376568ee65e34352fef
Oliver A. Kelton <oakelton@gmail.com>
2025-07-15 17:48:49 -0700

    Initialized database service

---
+++ Database/init-db.sql
@@ -0,0 +1,16 @@
+-- Stores table metadata --
+CREATE TABLE tables (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(256) NOT NULL,
+  width INTEGER NOT NULL CHECK (width > 0),
+  height INTEGER NOT NULL CHECK (height > 0)
+);
+
+-- Stores individual text cells per table --
+CREATE TABLE table_cells (
+  table_id BIGINT NOT NULL REFERENCES tables(id) ON DELETE CASCADE,
+  row_num INTEGER NOT NULL CHECK (row_num >= 0),
+  column_num INTEGER NOT NULL CHECK (column_num >= 0),
+  text TEXT NOT NULL,
+  PRIMARY KEY (table_id, row_num, column_num)
+);
