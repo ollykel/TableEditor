be0fc95fd769dfcd3795fdf6ddd2ff248cc4d9db
Oliver A. Kelton <oakelton@gmail.com>
2025-07-18 13:52:11 -0700

    Installed tailwindcss

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { fileURLToPath } from 'url';
 import path from 'path';
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // TypeScript workaround
 const __filename = fileURLToPath(import.meta.url);
@@ -9,7 +10,10 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
