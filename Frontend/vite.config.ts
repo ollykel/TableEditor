67786b4dbee299f6e2221f59ddf3bccad61878c7
Oliver A. Kelton <oakelton@gmail.com>
2025-07-18 16:44:23 -0700

    Configured proxy correctly on Frontend/vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -18,5 +18,15 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080/api',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
