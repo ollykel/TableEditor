0c1f1c888ee139b2d7dbba57a74c285f85741139
Oliver A. Kelton <oakelton@gmail.com>
2025-07-24 14:54:22 -0700

    Implemented PrivateRoute component

---
+++ Frontend/src/components/PrivateRoute.tsx
@@ -0,0 +1,17 @@
+import { Navigate} from "react-router-dom";
+import type { PropsWithChildren } from 'react';
+
+import { useAuth } from '@/context/AuthContext';
+
+export interface PrivateRouteProps {
+  fallbackUrl: string;
+}
+
+const PrivateRoute = (props: PropsWithChildren<PrivateRouteProps>): any => {
+  const { fallbackUrl, children } = props;
+  const { isAuthenticated } = useAuth();
+
+  return isAuthenticated() ? children : <Navigate to={fallbackUrl} replace />;
+}
+
+export default PrivateRoute;
