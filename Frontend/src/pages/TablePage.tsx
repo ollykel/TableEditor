c69a316bcff112fefed83b9e8258cbfacda1c527
Oliver A. Kelton <oakelton@gmail.com>
2025-07-30 18:23:10 -0700

    TablePage gives 404 not found page if api call returns 404

--- Frontend/src/pages/TablePage.tsx
+++ Frontend/src/pages/TablePage.tsx
@@ -4,30 +4,68 @@ import {
 } from '@tanstack/react-query';
 
 import { useAuthorizedFetch } from '@/context/AuthorizedFetchContext';
-import AuthedPage from '@/components/AuthedPage';
 import { WebSocketProvider } from '@/context/WebSocketContext';
+import AuthedPage from '@/components/AuthedPage';
 import TableEditor from '@/components/TableEditor';
+import NotFoundPage from '@/pages/NotFoundPage';
 
 import type TableProps from '@/types/TableProps';
+
+interface ErrorNotFound {
+  type: "not_found";
+}
+
+const ErrorNotFound = (): ErrorNotFound => ({ type: "not_found" });
+
+interface ErrorOther {
+  type: "other";
+  statusCode: number;
+  statusText: string;
+}
+
+const ErrorOther = (statusCode: number, statusText: string): ErrorOther => ({
+  type: "other", statusCode, statusText
+});
+
+type TableFetchError = ErrorNotFound | ErrorOther;
+
+interface TableFetchResult {
+  isPending: boolean;
+  error: TableFetchError;
+  tableProps: TableProps;
+  isFetching: boolean;
+}
 
 const TablePage = () => {
   const { tableId } = useParams();
   const { fetchAuthenticated } = useAuthorizedFetch();
-  const { isPending, error, data: tableProps, isFetching } = useQuery({
+  const { isPending, error, data: tableProps, isFetching }: TableFetchResult = useQuery({
+    retry: false,
     queryKey: ['tables', tableId],
     queryFn: async () => {
       const resp = await fetchAuthenticated(`/api/v1/tables/${tableId}`);
 
-      return await resp.json() as TableProps;
+      if (resp.status === 404) {
+        throw ErrorNotFound();
+      } else if (! resp.ok) {
+        throw ErrorOther(resp.status, resp.statusText);
+      } else {
+        return await resp.json() as TableProps;
+      }
     }
   });
 
   if (error) {
-    return (
-      <div>
-        <h1>Error: {`${error}`}</h1>
-      </div>
-    );
+    switch (error.type) {
+      case "not_found":
+        return (<NotFoundPage />);
+      default:
+        return (
+          <div>
+            <h1>Error: {error.statusText} ({error.statusCode})</h1>
+          </div>
+        );
+    }// end switch (error.type)
   } else if (isPending || isFetching) {
       <div>
         <h1>Loading ...</h1>
