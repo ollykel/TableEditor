38aa1cf61289e1d8941e5a67486943adc2403c36
Oliver A. Kelton <oakelton@gmail.com>
2025-08-03 19:30:26 -0700

    Make Title component link to landing page

--- Frontend/src/components/Title.tsx
+++ Frontend/src/components/Title.tsx
@@ -4,10 +4,16 @@
 //
 // =============================================================================
 
+import { Link } from 'react-router';
+
+import config from '@/app.config';
+
 const Title = (): React.JSX.Element => {
   return (
     <div>
-      <h1 className="text-center text-4xl font-bold font-serif">Table Editor</h1>
+      <Link to={config.routes.index}>
+        <h1 className="text-center text-4xl font-bold font-serif">Table Editor</h1>
+      </Link>
     </div>
   );
 };
