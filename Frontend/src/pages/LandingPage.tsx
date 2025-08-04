dedbe3664d33b6b194e311a69fa4ed307f506a8e
Oliver A. Kelton <oakelton@gmail.com>
2025-08-03 19:41:58 -0700

    Added little blurb to landing page, made the login form on the landing page a card

--- Frontend/src/pages/LandingPage.tsx
+++ Frontend/src/pages/LandingPage.tsx
@@ -1,10 +1,12 @@
-import { Link } from "react-router";
 import { useNavigate } from 'react-router-dom';
 
 import config from '@/app.config';
 
 import Page from '@/components/Page';
 import Title from '@/components/Title';
+import Card from '@/components/Card';
+import Link from '@/components/Link';
+import H2 from '@/components/H2';
 import LoginForm from '@/components/LoginForm';
 import { useAuth } from '@/context/AuthContext';
 
@@ -30,10 +32,20 @@ const LandingPage = (): React.JSX.Element => {
   return (
     <Page title="Welcome!">
       <Title />
-      <LoginForm onSubmit={handleSubmit} />
-      <Link to={config.routes.createAccount}>
-        Create Account
-      </Link>
+
+      <div className="flex flex-col text-center items-center">
+        <p className="w-3/5">
+          Want a spreadsheet editor with a focus on text editing and collaboration? Look no further! The Table Editor is here.
+        </p>
+        {/** Login card **/}
+        <Card className="w-1/2">
+          <H2>Login</H2>
+          <LoginForm onSubmit={handleSubmit} />
+          <p className="text-center mt-2">
+            Or <Link to={config.routes.createAccount}>Create Account</Link>
+          </p>
+        </Card>
+      </div>
     </Page>
   );
 };
